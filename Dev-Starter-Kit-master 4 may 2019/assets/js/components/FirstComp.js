cc03af39e1a8b06620dc8da694193c4847e707c1
Patrick Ly <patrickkimly@gmail.com>
2019-07-24 15:55:11 -0700

    Build dropdown menu 16:00

--- Dev-Starter-Kit-master 4 may 2019/assets/js/components/FirstComp.js
+++ Dev-Starter-Kit-master 4 may 2019/assets/js/components/FirstComp.js
@@ -17,7 +17,21 @@ class Layout extends Component {
 					<li>
 						Products
 						<ul>
-							<li>item1</li>
+							<li>
+								item1
+								<ul>
+									<li>
+										item1
+										<ul>
+											<li>item1</li>
+											<li>item2</li>
+											<li>item3</li>
+										</ul>
+									</li>
+									<li>item2</li>
+									<li>item3</li>
+								</ul>
+							</li>
 							<li>item2</li>
 							<li>item3</li>
 						</ul>
